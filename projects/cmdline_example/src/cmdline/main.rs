f37fbd00b9088685ecabdaa980bb4b6a70cc4bae
deeso <adam.pridgen@thecoverofnight.com>
2014-05-25 22:48:37 -0500

    Adding some basic code

---
+++ projects/cmdline_example/src/cmdline/main.rs
@@ -0,0 +1,46 @@
+extern crate getopts;
+use getopts::{optopt,optflag,getopts,OptGroup};
+use std::os;
+
+fn do_work(inp: &str, out: Option<StrBuf>) {
+    println!("{}", inp);
+    match out {
+        Some(x) => println!("{}", x),
+        None => println!("No Output"),
+    }
+}
+
+fn print_usage(program: &str, _opts: &[OptGroup]) {
+    println!("Usage: {} [options]", program);
+    println!("-o\t\tOutput");
+    println!("-h --help\tUsage");
+}
+
+fn main() {
+    let args: Vec<StrBuf> = os::args().iter()
+                                      .map(|x| x.to_strbuf())
+                                      .collect();
+
+    let program = args.get(0).clone();
+
+    let opts = [
+        optopt("o", "", "set output file name", "NAME"),
+        optflag("h", "help", "print this help menu")
+    ];
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => { m }
+        Err(f) => { fail!(f.to_err_msg()) }
+    };
+    if matches.opt_present("h") {
+        print_usage(program.as_slice(), opts);
+        return;
+    }
+    let output = matches.opt_str("o");
+    let input = if !matches.free.is_empty() {
+        (*matches.free.get(0)).clone()
+    } else {
+        print_usage(program.as_slice(), opts);
+        return;
+    };
+    do_work(input.as_slice(), output);
+}
---
+++ projects/cmdline_example/src/cmdline/options.rs
@@ -0,0 +1,4 @@
+static NUM_OPTIONS : int = 1;
+static USAGE : &'static str = "usage string";
+//static SOME_STRUCT : MyStruct = MyStruct { number: 10, string: &'static "Some string" };
+//static mut db : Option<sqlite::database::Database> = None;
